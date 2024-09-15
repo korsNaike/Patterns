3673f70196f4c17a431dd599f657d3132657fc5f
korsNaike <kirillgrinev2003@mail.ru>
2024-09-16 00:22:53 +0300

    Л1/З4. Добавлены вторичные конструкторы

--- src/main/kotlin/pattern/Student.kt
+++ src/main/kotlin/pattern/Student.kt
@@ -10,6 +10,20 @@ class Student(
     var email: String? = null,
     var git: String? = null
 ) {
+
+    constructor(
+        id: Int,
+        lastName: String,
+        firstName: String,
+        middleName: String
+    ) : this(id, lastName, firstName, middleName, null, null, null, null)
+
+    constructor(
+        lastName: String,
+        firstName: String,
+        middleName: String
+    ) : this(0, lastName, firstName, middleName, null, null, null, null)
+
     override fun toString(): String {
         return "Student(id=$id, name='$firstName $middleName $lastName', " +
                 "phone=$phone, telegram=$telegram, email=$email, git=$git)"
