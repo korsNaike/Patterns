f3a687c469e48a59eb675d11332bc25295d6721a
korsNaike <kirillgrinev2003@mail.ru>
2024-09-17 12:16:13 +0300

    Л2/З2.2. Добавлены методы получения количества столбцов и строк, и элемента

--- src/main/kotlin/pattern/Data_table.kt
+++ src/main/kotlin/pattern/Data_table.kt
@@ -6,4 +6,15 @@ class Data_table<T> private constructor(private val data: Array<Array<T>>) {
             return Data_table(data)
         }
     }
+
+    fun getElement(row: Int, column: Int): T {
+        require(row in data.indices && column in data[0].indices) {
+            "Индексы выходят за пределы таблицы"
+        }
+        return data[row][column]
+    }
+
+    fun getColumnCount(): Int = if (data.isNotEmpty()) data[0].size else 0
+
+    fun getRowCount(): Int = data.size
 }
