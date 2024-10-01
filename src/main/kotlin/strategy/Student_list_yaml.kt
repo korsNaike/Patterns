ee66630df0a584761f18609e6b8f2346c95b6448
korsNaike <kirillgrinev2003@mail.ru>
2024-10-01 16:06:30 +0300

    Л3/З4. Проведен рефакторинг, выделен суперкласс Student_base_list

--- src/main/kotlin/strategy/Student_list_yaml.kt
+++ src/main/kotlin/strategy/Student_list_yaml.kt
@@ -10,7 +10,7 @@ import org.korsnaike.student.Student
 import org.korsnaike.student.Student_short
 import java.io.File
 
-class Student_list_yaml(private var students: MutableList<Student> = mutableListOf()) {
+class Student_list_yaml(students: MutableList<Student> = mutableListOf()) : Student_base_list(students) {
 
     constructor() : this(mutableListOf())
 
@@ -18,7 +18,7 @@ class Student_list_yaml(private var students: MutableList<Student> = mutableList
         read_from_file(filePath)
     }
 
-    fun read_from_file(filePath: String) {
+    override fun read_from_file(filePath: String) {
         val file = File(filePath)
         if (!file.exists() || !file.isFile) {
             throw IllegalArgumentException("Некорректный адрес файла: $filePath")
@@ -31,7 +31,7 @@ class Student_list_yaml(private var students: MutableList<Student> = mutableList
         }
     }
 
-    fun write_to_file(directory: String, fileName: String) {
+    override fun write_to_file(directory: String, fileName: String) {
         val file = File(directory, fileName)
 
         try {
@@ -41,71 +41,4 @@ class Student_list_yaml(private var students: MutableList<Student> = mutableList
             throw IllegalArgumentException("Ошибка при записи в файл JSON: ${e.message}")
         }
     }
-
-    fun findById(id: Int): Student {
-        return students.first { it.id == id }
-    }
-
-    /**
-     * Получить список k по счету n объектов класса Student_short
-     *
-     * @param n - индекс с которого начинать выборку (должен быть 0 или больше)
-     * @param k - количество элементов для выборки (должно быть больше 0)
-     *
-     * @return Список объектов Student_short
-     */
-    fun get_k_n_student_short_list(n: Int, k: Int): Data_list<Student_short> {
-        require(n >= 0) { "Индекс n должен быть больше или равен 0." }
-        require(k > 0) { "Количество k должно быть больше 0." }
-
-        return Data_list_student_short(students
-            .drop(n)
-            .take(k)
-            .map { Student_short(it) }
-        )
-    }
-
-    /**
-     * Сортировка списка по фамилии и инициалам
-     */
-    fun orderStudentsByLastNameInitials() {
-        orderStudents(compareBy { it.getLastNameWithInitials() })
-    }
-
-    /**
-     * Сортировка списка
-     */
-    fun orderStudents(comparator: Comparator<Student>) {
-        students.sortedWith(comparator)
-    }
-
-    /**
-     * Добавление объекта
-     */
-    fun add(student: Student) {
-        val nextId = (students.maxByOrNull { it.id }?.id ?: 0) + 1
-        student.id = nextId
-
-        students.addLast(student)
-    }
-
-    /**
-     * Заменить объект по ID
-     */
-    fun replaceById(student: Student, id: Int) {
-        student.id = id
-        students.replaceAll { if (it.id == id) student else it }
-    }
-
-    /**
-     * Удалить по ID
-     */
-    fun removeById(id: Int) {
-        students.removeIf { it.id == id }
-    }
-
-    /**
-     * Получить количество объектов в списке
-     */
-    fun get_student_short_count(): Int = students.count()
 }
